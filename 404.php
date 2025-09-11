de715c9e43e0a0d7dc93d412aa23478ad53de996
berti <berti@CLASSES.LOCAL>
2025-09-11 09:39:38 +0200

    404

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+    header("HTTP/1.0 404 Not Found");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Page introuvable!</h1>
+    <a href="index.php">Revenir à l'accueil</a>
+</body>
+</html>
